055d606a475e335a6551400419ae7f16144a758d
Estiak Ahmed Sourav <37806949+easourav@users.noreply.github.com>
2020-10-14 01:18:50 +0600

    added packages

--- app/src/main/java/com/sourav/tenmuniteschool/activitys/MainActivity.kt
+++ app/src/main/java/com/sourav/tenmuniteschool/activitys/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sourav.tenmuniteschool
+package com.sourav.tenmuniteschool.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sourav.tenmuniteschool.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/sourav/tenmuniteschool/adapters/HomeMainAdapter.java
@@ -0,0 +1,4 @@
+package com.sourav.tenmuniteschool.adapters;
+
+public class HomeMainAdapter {
+}
