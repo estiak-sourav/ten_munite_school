c96bec7ee824c97fae31118d8ebae1226264be90
Estiak Ahmed Sourav <37806949+easourav@users.noreply.github.com>
2020-10-14 01:36:33 +0600

    added recyclerview item layout

---
+++ app/src/main/java/com/sourav/tenmuniteschool/activitys/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sourav.tenmuniteschool.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sourav.tenmuniteschool.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
