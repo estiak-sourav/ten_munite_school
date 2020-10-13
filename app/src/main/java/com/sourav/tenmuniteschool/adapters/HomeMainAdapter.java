43d6269bf95184d002227d05e2882983c344de69
Estiak Ahmed Sourav <37806949+easourav@users.noreply.github.com>
2020-10-14 01:26:26 +0600

    added main adapter and material design dependency

--- app/src/main/java/com/sourav/tenmuniteschool/adapters/HomeMainAdapter.java
+++ app/src/main/java/com/sourav/tenmuniteschool/adapters/HomeMainAdapter.java
@@ -1,4 +1,31 @@
 package com.sourav.tenmuniteschool.adapters;
 
-public class HomeMainAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeMainAdapter extends RecyclerView.Adapter<HomeMainAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public HomeMainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeMainAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
